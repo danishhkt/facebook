4e7afcd5873d6a46682c8c88f7af91210ada7182
danishhkt <danishdan204@gmail.com>
2019-11-13 10:00:13 +0300

    first name

--- fb/urls.py
+++ fb/urls.py
@@ -1,5 +1,5 @@
-from django import path
-from . import views
-url patternd = [
+from django.urls import path
+from . import views 
+urlpatterns = [
     path('',views.fn_test)
 ]
--- fb/views.py
+++ fb/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def fn_test(request):
-    return HttpResponse('welcome')
+    return render(request,'facebookHomePage.html')
